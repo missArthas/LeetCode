9a3beeb297ace64c906d4a5722b6bdc87bfcc6c8
hsh黄苏豪 <shhuang@ctrip.com>
2017-03-28 16:32:52 +0800

    268,447

---
+++ PythonVersion/easy/Q268MissingNumber.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l=len(nums)
+        return l*(l+1)/2-sum(nums)
+
+object=Solution()
+print object.missingNumber([1,2])
